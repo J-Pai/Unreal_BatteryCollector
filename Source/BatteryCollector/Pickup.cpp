e93fd1fadb24acebf293616f9db199391daf2950
j-pai <jesse.pai@gmail.com>
2018-02-04 22:40:32 -0500

    Previously unstaged changes required for prior commit.

--- Source/BatteryCollector/Pickup.cpp
+++ Source/BatteryCollector/Pickup.cpp
@@ -5,19 +5,29 @@
 #include "BatteryCollector.h"
 
 // Sets default values
-APickup::APickup()
-{
+APickup::APickup() {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
 
+	// Initialize bIsActive to be true.
+	bIsActive = true;
+
 	//Create the static mesh component
-	PickupMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PickupMesh"));
-	SetRootComponent(PickupMesh);
+	pickupMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PickupMesh"));
+	SetRootComponent(pickupMesh);
 }
 
 // Called when the game starts or when spawned
-void APickup::BeginPlay()
-{
+void APickup::BeginPlay() {
 	Super::BeginPlay();
 }
 
+// Returns active state
+bool APickup::IsActive() {
+	return bIsActive;
+}
+
+// Changes active state
+void APickup::SetActive(bool newPickupState) {
+	bIsActive = newPickupState;
+}
--- Source/BatteryCollector/Pickup.h
+++ Source/BatteryCollector/Pickup.h
@@ -14,20 +14,26 @@ class BATTERYCOLLECTOR_API APickup : public AActor
 public:	
 	// Sets default values for this actor's properties
 	APickup();
+	/** Return the mesh for the Pickup */
+	FORCEINLINE class UStaticMeshComponent * GetMesh() const {
+		return pickupMesh;
+	}
+	/** Return whether the pickup is active */
+	UFUNCTION(BlueprintPure, Category = "Pickup")
+	bool IsActive();
+	/** Allows other classes to change if the pickup is active  */
+	UFUNCTION(BlueprintCallable, Category = "Pickup")
+	void SetActive(bool newPickupState);
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
-public:	
-	/* Return the mesh for the Pickup */
-	FORCEINLINE class UStaticMeshComponent * GetMesh() const {
-		return PickupMesh;
-	}
+	/** True when the pickup can be used, false when pickup can no longer be used.*/
+	bool bIsActive;
 
 private:
-	/* Static mesh to represent the pickup in the level */
+	/** Static mesh to represent the pickup in the level */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Pickup", meta = (AllowPrivateAccess = "true"))
-	class UStaticMeshComponent * PickupMesh;
+	class UStaticMeshComponent * pickupMesh;
 	
 };
